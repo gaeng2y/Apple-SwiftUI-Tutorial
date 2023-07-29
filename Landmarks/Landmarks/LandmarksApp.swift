02b19139b64328e0b0917c02e2b197a1117e5b09
gaeng2y <gaeng2y@gmail.com>
2023-07-29 13:44:08 +0900

    Section 2. Drawing Paths and Shapes

--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    // @StateObject 특성을 사용하여 앱 수명 동안 한 번만 지정된 속성에 대한 모델 개체를 초기화합니다. 여기에 표시된 것처럼 앱 인스턴스에서 속성을 사용할 때와 보기에서 속성을 사용할 때 마찬가지입니다.
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
---
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,46 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Kyeongmo Yang on 2023/07/27.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    // 앱의 많은 뷰와 공유해야하는 데이터의 경우 SwiftUI는 EnvironmentObject속성 래퍼를 제공합니다.
+    // 이를 통해 필요한 곳 ​​어디에서나 모델 데이터를 공유 할 수 있으며, 데이터가 변경 될 때 뷰가 자동으로 업데이트 된 상태로 유지됩니다.
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
